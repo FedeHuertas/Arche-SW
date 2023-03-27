24b5d9e4e737f1869600c53896c711bd3eb55655
FedeHuertas <federicohuertas@outlook.com>
2023-03-26 22:17:00 -0300

    total price in cart screen

--- app/context/CartContext.jsx
+++ app/context/CartContext.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 
 const cartContext = createContext([]);
 
@@ -6,8 +6,14 @@ export const useCartContext = () => useContext(cartContext);
 
 const CartProvider = ({children}) => {
     const [cart, setCart] = useState([]);
+    const [sum, setSum] = useState(0)
 
-    const clearCart = () => setCart([]);
+    const sumCart = () => {
+        setSum(cart.reduce((acc, i) => acc+i.price, 0))
+    }
+
+    const clearCart = () => setCart([])
+
 
     const removeProduct = (id) => setCart(cart.filter(p => p.id != id))
 
@@ -15,13 +21,18 @@ const CartProvider = ({children}) => {
         cart.find(p => p.id == product.id) ?? setCart([...cart, product])
     }
 
+    useEffect(() => {
+        sumCart()
+    }, [cart])
+
     return(
         <cartContext.Provider
             value={{
                 clearCart,
                 removeProduct,
                 addProduct,
-                cart
+                cart,
+                sum
             }}
         >
             {children}
--- app/screens/CartScreen.js
+++ app/screens/CartScreen.js
@@ -1,30 +1,40 @@
-import { FlatList, Text, View, StyleSheet} from "react-native";
+import { FlatList, Text, View, StyleSheet, Pressable} from "react-native";
 import ProductItem from "../components/ProductCard";
 import theme from "../theme";
 import { useCartContext } from "../context/CartContext";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Button from "../components/Button";
 
 
 const CartScreen = () => {
 
-    const { cart } = useCartContext()
+    const { cart, sum } = useCartContext()
 
     return (
         cart.length ?
-        <FlatList
-            data={cart}
-            renderItem={({item: p}) => (
-                <ProductItem
-                    id={p.id}
-                    title={p.title}
-                    description={p.description}
-                    price={p.price}
-                    thumbnail={p.thumbnail}
-                    inCart={true}
-                />
-            )}
-        >
-        </FlatList>
+        <>
+{/*             <Pressable>
+                <MaterialCommunityIcons name="delete-circle-outline" size={24} color="black" />
+            </Pressable> */}
+            <FlatList
+                data={cart}
+                renderItem={({item: p}) => (
+                    <ProductItem
+                        id={p.id}
+                        title={p.title}
+                        description={p.description}
+                        price={p.price}
+                        thumbnail={p.thumbnail}
+                        inCart={true}
+                    />
+                )}
+            >
+            </FlatList>
+            <View style={styles.footer}>
+                <Text style={styles.total}>Total: ${sum}</Text>
+                <Button>Buy now</Button>
+            </View>
+        </>
         :
         <View style={theme.center}>
             <MaterialCommunityIcons name="cart-remove" size={100} color={theme.colors.inactive} />
@@ -41,5 +51,15 @@ const styles = StyleSheet.create({
         color: theme.colors.inactive,
         fontWeight: 'bold',
         margin: 8
+    },
+    footer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginHorizontal: 25
+    },
+    total: {
+        fontSize: 18,
+        fontWeight: 'bold'
     }
 })
